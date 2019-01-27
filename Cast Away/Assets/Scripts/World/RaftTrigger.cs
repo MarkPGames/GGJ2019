26d18f3b3bd6344bf4630ad1bb94da03f6bc285b
Mark Phillips <markelvisphillips@gmail.com>
2019-01-27 12:17:00 +1100

    Raft Trigger

---
+++ Cast Away/Assets/Scripts/World/RaftTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaftTrigger : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    bool go = false;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        go = true;
+        
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) < 0.001f)
+        {
+            Vector3.MoveTowards(transform.position, target.position, 5f * Time.deltaTime);
+        }
+    }
+
+}
